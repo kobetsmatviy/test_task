bff215e61ab6c42123add6b31c2054e658d4dea5
Kobets Matviy <kobetsmatviy@gmail.com>
2020-07-21 17:49:57 +0300

    create the broser-sync module for html reload and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 
 function style() {
-    return gulp.src('./less/*.less')
+    return gulp.src('./less/**/*.less')
                 .pipe(less())
                 .pipe(gulp.dest('./css'))
+                .pipe(browserSync.stream())
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            index: 'main.html'
+        }
+    })
+    gulp.watch('./less/*.less', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
