c09f66e1debcd51a79f41265688d06bdf7f0ca23
Kobets Matviy <kobetsmatviy@gmail.com>
2020-07-23 15:49:53 +0300

    completion of the task

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    //#### Initialize preloader
+    $(window).on('load', function () {
+        window.setTimeout(function () {
+          $('.preloader').fadeOut().end().delay(400).fadeOut('slow');
+        }, 500);
+      });
+
     //#### The toggle func
     var toggleButton = $('.nav__toggle');
     var toggleBlock = $('.nav__list');
@@ -26,12 +33,20 @@ $(document).ready(function() {
     
         if ((x >= offsetBody.left && x <= offsetBody.left + offsetBody.width) && (y >= offsetBody.top && y <= offsetBody.top + offsetBody.height)) {
             $('#mouse').css('left', x+'px').css('top', y+'px');
+            $('#mouseMessage').css('left', (x+35)+'px').css('top', (y-10)+'px').css('opacity', '0').css('transition', '.5s opacity ease-in-out');
         }
         if ((x >= offsetTarget.left && x <= offsetTarget.left + offsetTarget.width) && (y >= offsetTarget.top && y <= offsetTarget.top + offsetTarget.height)) {
-            $('#mouseMessage').css('left', (x+35)+'px').css('top', (y-10)+'px').css('opacity', '1').css('transition', '1s opacity ease-in-out');
+            $('#mouseMessage').css('left', (x+35)+'px').css('top', (y-10)+'px').css('opacity', '1').css('transition', '.3s opacity ease-in-out');
         }
     });
     $target.mouseleave(function() {
         $('#mouseMessage').css('opacity', '0').css('transition', '.5s opacity ease-in-out');
-    }); 
+    });
+    // The link hover action
+    $('a').mouseover(function() {
+        $('#mouse').css('border-color', '#580300').css('transition', 'border-color .3s ease-in-out');
+    });
+    $('a').mouseleave(function() {
+        $('#mouse').css('border-color', '#E5E5E5').css('transition', 'border-color .3s ease-in-out');
+    });
 });
